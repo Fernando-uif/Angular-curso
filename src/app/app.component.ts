d9370c58f9a726d0b0b753f58d02e8a348b4c841
fernando-ui <fst-96@hotmail.com>
2022-12-11 13:24:07 -0600

    Add the events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-root',
-  templateUrl:'./app.component.html',
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'Contador App';
+  title: string = 'Contador App';
+  numero: number = 0;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
 }
